5257b17680bc71fa551b139e6007ca5c66e1bd31
TomaNikolov <on.tomas@gmail.com>
2021-03-25 16:26:12 +0200

    [CHAT-2374]Fix node-fetch error

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const fetch = require('node-fetch');
-const FetchError = require('node-fetch/lib/fetch-error');
 
 const MAX_RETRIES = 5;
 
 const _fetch = (url, options, retries) => {
     return fetch(url, options)
         .catch(err => {
-            if (err instanceof FetchError || retries < MAX_RETRIES) {
+            if (err instanceof fetch.FetchError || retries < MAX_RETRIES) {
                 return _fetch(url, options, retries + 1);
             } else {
                 throw err;
